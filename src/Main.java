1919e8344f3ea72d1a0d0bfaac2447f8996ee842
Andrew Vinals <Vinals.aj@gmail.com>
2024-06-03 19:56:19 -0400

    Meet your chat bot

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("test");
+        System.out.println("Hello! My name is Sam.");
+        System.out.println("I was created in 2024.");
     }
 }
