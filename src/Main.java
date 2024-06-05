edf3bebd18089610c1f939032857a2eef961f280
Andrew Vinals <Vinals.aj@gmail.com>
2024-06-05 08:23:22 -0400

    Added scanner to take name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello! My name is Sam.");
         System.out.println("I was created in 2024.");
+        System.out.println("Please, remind me your name.");
+
+        String name = sc.nextLine();
+
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
